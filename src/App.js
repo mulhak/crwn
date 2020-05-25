5d408f6b9e9e1d9e8d5fcec4edda73ecbee96862
mulhak <mulhakieran@gmail.com>
2020-05-25 12:12:47 +0100

    broken doem to smaller components and added some css styles not fully completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './homepage.styles.scss';
 
 import './App.css';
-import HomePage from './homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+
 
 function App() {
   return (
